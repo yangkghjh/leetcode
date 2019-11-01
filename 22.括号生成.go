ceaacf68edb56174e2068c4c0db7c99f80146fa5
root <root@DESKTOP-DUP1VH0.localdomain>
2019-11-01 18:13:56 +0800

    [22] 括号生成

---
+++ 22.括号生成.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=22 lang=golang
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+func generateParenthesis(n int) []string {
+	results := []string{}
+	return backtrack(results, "", 0, 0, n)
+}
+
+func backtrack(results []string, stack string, open int, close int, max int) []string {
+	if len(stack) == max*2 {
+		results = append(results, stack)
+		return results
+	}
+
+	if open < max {
+		results = backtrack(results, stack+"(", open+1, close, max)
+	}
+	if close < open {
+		results = backtrack(results, stack+")", open, close+1, max)
+	}
+	return results
+}
+
+// @lc code=end
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	start := time.Now()
-	fmt.Println(mergeTwoLists(&ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}, &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}))
+	fmt.Println(generateParenthesis(3))
 	end := time.Now()
 	fmt.Println(end.Sub(start))
 }
