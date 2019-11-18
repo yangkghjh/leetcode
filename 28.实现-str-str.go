e176e7a1f0b51270a8ebd3a756954e99f50f4eeb
yangbin <yangkghjh@gmail.com>
2019-11-18 18:50:36 +0800

    [28] 实现 strStr()

---
+++ 28.实现-str-str.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	lenght := len(haystack) - len(needle)
+	if lenght < 0 {
+		return -1
+	}
+	targetLength := len(needle)
+
+	for i := 0; i <= lenght; i++ {
+		if haystack[i:i+targetLength] == needle {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// @lc code=end
