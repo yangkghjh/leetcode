ef55221a858596d479405230111253a79739054a
yangbin <yangkghjh@gmail.com>
2019-11-20 17:59:34 +0800

    [33] 搜索旋转排序数组

---
+++ 33.搜索旋转排序数组.go
@@ -0,0 +1,52 @@
+package main
+
+// import "fmt"
+
+/*
+ * @lc app=leetcode.cn id=33 lang=golang
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	if nums[0] == target {
+		return 0
+	}
+	if nums[len(nums)-1] == target {
+		return len(nums) - 1
+	}
+
+	return binarySearch(nums, target, 0, len(nums)-1)
+}
+
+func binarySearch(nums []int, target, start, end int) int {
+	mid := (start + end) / 2
+	if nums[mid] == target {
+		return mid
+	}
+
+	// fmt.Println(start, end)
+	if end-start <= 1 {
+		return -1
+	}
+
+	if nums[start] < nums[mid] { // 前半段有序
+		if target < nums[mid] && target > nums[start] { // 在前半段
+			return binarySearch(nums, target, start, mid)
+		}
+		// 不在前半段
+		return binarySearch(nums, target, mid, end)
+	} else {
+		if target > nums[mid] && target < nums[end] { // 在后半段
+			return binarySearch(nums, target, mid, end)
+		}
+		// 不在后半段
+		return binarySearch(nums, target, start, mid)
+	}
+}
+
+// @lc code=end
---
+++ 33.搜索旋转排序数组_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSearch(t *testing.T) {
+	Convey("[33] 搜索旋转排序数组", t, func() {
+		So(search([]int{1, 2, 3, 4, 5}, 2), ShouldEqual, 1)
+		So(search([]int{1, 2, 3, 4, 5}, 4), ShouldEqual, 3)
+		So(search([]int{1, 2, 3, 4, 5}, 5), ShouldEqual, 4)
+		So(search([]int{4, 5, 1, 2, 3}, 5), ShouldEqual, 1)
+		So(search([]int{2, 3, 4, 5, 1}, 3), ShouldEqual, 1)
+
+		So(search([]int{2, 3, 4, 5, 1}, 2), ShouldEqual, 0)
+		So(search([]int{2, 3, 4, 5, 1}, 1), ShouldEqual, 4)
+		So(search([]int{2, 3, 4, 5, 1}, 4), ShouldEqual, 2)
+		So(search([]int{2, 3, 4, 5, 1}, 7), ShouldEqual, -1)
+		So(search([]int{2, 3, 4, 6, 1}, 5), ShouldEqual, -1)
+		So(search([]int{}, 5), ShouldEqual, -1)
+	})
+}
