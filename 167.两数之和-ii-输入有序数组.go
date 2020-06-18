595db5dafd1b8ba38b2f0fbb7135a71973304981
杨斌 <yangkghjh@gmail.com>
2020-06-18 22:19:20 +0800

    [167] 两数之和 II - 输入有序数组

---
+++ 167.两数之和-ii-输入有序数组.go
@@ -0,0 +1,24 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=167 lang=golang
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+func twoSum(numbers []int, target int) []int {
+	index1, index2 := 0, len(numbers)-1
+
+	for {
+		if numbers[index1]+numbers[index2] == target {
+			return []int{index1 + 1, index2 + 1}
+		} else if numbers[index1]+numbers[index2] < target {
+			index1++
+		} else {
+			index2--
+		}
+	}
+}
+
+// @lc code=end
