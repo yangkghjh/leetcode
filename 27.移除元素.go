fbc9cec1556eb704a259843cb3ff0d3ccb9a93cd
杨斌 <yangkghjh@gmail.com>
2020-06-22 14:44:29 +0800

    [27] 移除元素

---
+++ 27.移除元素.go
@@ -0,0 +1,25 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=27 lang=golang
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+func removeElement(nums []int, val int) int {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+
+	return slow
+}
+
+// 时间复杂度： O(n)
+// 空间复杂度： O(1)
+
+// @lc code=end
