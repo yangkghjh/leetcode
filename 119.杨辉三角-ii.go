04ccf2ab494c4c298da3950ed70b6f89053832b0
杨斌 <yangkghjh@gmail.com>
2020-06-22 16:51:40 +0800

    [119] 杨辉三角 II

---
+++ 119.杨辉三角-ii.go
@@ -0,0 +1,28 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=119 lang=golang
+ *
+ * [119] 杨辉三角 II
+ */
+
+// @lc code=start
+func getRow(rowIndex int) []int {
+	ans := make([]int, rowIndex+1)
+	ans[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		ans[i] = 1
+		left, right := ans[0], ans[1]
+		for j := 1; j < i; j++ {
+			ans[j] = left + right
+			left, right = right, ans[j+1]
+		}
+	}
+
+	return ans
+}
+
+// 时间复杂度 O(n2)
+// 空间复杂度 O(k)
+
+// @lc code=end
