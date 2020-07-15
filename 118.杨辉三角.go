12bbb6d5f473b6e6ef87265d064c2c868baee8f2
杨斌 <yangkghjh@gmail.com>
2020-07-15 17:09:50 +0800

    [118] 杨辉三角 递归写法

--- 118.杨辉三角.go
+++ 118.杨辉三角.go
@@ -7,7 +7,32 @@ package leetcode
  */
 
 // @lc code=start
+// 递归写法
 func generate(numRows int) [][]int {
+	if numRows == 0 {
+		return [][]int{}
+	}
+	if numRows == 1 {
+		return [][]int{{1}}
+	}
+
+	row := make([]int, numRows)
+	result := generate(numRows - 1)
+	prev := result[len(result)-1]
+
+	row[0] = 1
+	row[numRows-1] = 1
+
+	for i := 1; i < numRows-1; i++ {
+		row[i] = prev[i-1] + prev[i]
+	}
+
+	result = append(result, row)
+
+	return result
+}
+
+func generateCommon(numRows int) [][]int {
 	res := make([][]int, numRows)
 	for row := 0; row < numRows; row++ {
 		r := make([]int, row+1)
@@ -21,5 +46,5 @@ func generate(numRows int) [][]int {
 
 	return res
 }
-// @lc code=end
 
+// @lc code=end
