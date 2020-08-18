7b6dd64678d144bfb9b2d712c566900666a45c06
杨斌 <yangkghjh@gmail.com>
2020-08-18 18:11:56 +0800

    53-I.在排序数组中查找数字

--- 剑指Offer/53-I.在排序数组中查找数字.go
+++ 剑指Offer/53-I.在排序数组中查找数字.go
@@ -3,15 +3,34 @@ package offer
 // 统计一个数字在排序数组中出现的次数。
 
 func search(nums []int, target int) int {
-	ans := 0
-
-	for _, num := range nums {
-		if num == target {
-			ans++
-		} else if ans > 0 {
-			break
-		}
+	l := len(nums)
+	if l == 0 {
+		return 0
 	}
 
-	return ans
+	i := 0
+	j := l - 1
+	for i <= j {
+		mid := j - (j-i)/2
+		if nums[mid] <= target {
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+	}
+	right := i
+
+	i = 0
+	j = l - 1
+	for i <= j {
+		mid := j - (j-i)/2
+		if nums[mid] < target {
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+	}
+	left := j
+
+	return right - left - 1
 }
