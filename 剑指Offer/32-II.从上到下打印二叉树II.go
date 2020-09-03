071f873aca61b323a55351dfd4da9dc3f9f631c2
Yang Bin <yangkghjh@gmail.com>
2020-09-03 19:43:45 +0800

    32-II.从上到下打印二叉树II

---
+++ 剑指Offer/32-II.从上到下打印二叉树II.go
@@ -0,0 +1,33 @@
+package offer
+
+// 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+	stack := []*TreeNode{root}
+	ans := [][]int{}
+
+	for len(stack) > 0 {
+		next := []*TreeNode{}
+		vals := []int{}
+
+		for _, node := range stack {
+			if node != nil {
+				vals = append(vals, node.Val)
+				if node.Left != nil {
+					next = append(next, node.Left)
+				}
+				if node.Right != nil {
+					next = append(next, node.Right)
+				}
+			}
+		}
+
+		stack = next
+		ans = append(ans, vals)
+	}
+
+	return ans
+}
